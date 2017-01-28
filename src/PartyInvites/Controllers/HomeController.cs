b86cdeb59dc72c461a0db7a270cd2683a0ade2bf
Jon Edwards <jkedwards@me.com>
2017-01-27 21:29:14 -0800

    add persistence (memory) and validation

--- src/PartyInvites/Controllers/HomeController.cs
+++ src/PartyInvites/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
 
 namespace PartyInvites.Controllers {
     public class HomeController : Controller {
@@ -8,8 +10,22 @@ namespace PartyInvites.Controllers {
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
         }
+        [HttpGet]
         public ViewResult RsvpForm() {
             return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            if(ModelState.IsValid) {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else {
+                // there is a validation error
+                return View();
+            }
+        }
+        public ViewResult ListResponses() {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
